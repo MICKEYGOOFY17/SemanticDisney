ab843247cb5f336228977d48a9096f67e677a3d6
unknown <syuvashree1995@gmail.com>
2017-01-13 15:32:02 +0530

    added google fonts and aligned the code for readability

--- js/script.js
+++ js/script.js
@@ -1,56 +1,57 @@
-setInterval(scroll,5000);
-  function scroll(){
-    $('.ui.shape').shape('flip over');
-  }
+setInterval(scroll, 5000);
 
-function signup(){
-$('.signup').modal('show');
-$('.ui.checkbox').checkbox();
+function scroll() {
+    $('.ui.shape').shape('flip over');
 }
 
-function signin(){
-$('.signin').modal('show');
-$('.ui.checkbox').checkbox();
+function signup() {
+    $('.signup').modal('show');
+    $('.ui.checkbox').checkbox();
+}
+
+function signin() {
+    $('.signin').modal('show');
+    $('.ui.checkbox').checkbox();
 }
 
 function check() {
-  $('.inform')
-  .form({
-    fields: {
-      username : 'empty',
-      password : ['minLength[6]', 'empty'],
-    }
-  });
+    $('.inform')
+        .form({
+            fields: {
+                username: 'empty',
+                password: ['minLength[6]', 'empty'],
+            }
+        });
 }
 
 function checksign() {
-  $('.upform')
-  .form({
-    fields: {
-      username : 'empty',
-      password : ['minLength[6]', 'empty'],
-      email : 'empty',
-      gender   : 'empty',
-      terms    : 'checked'
-    }
-  });
+    $('.upform')
+        .form({
+            fields: {
+                username: 'empty',
+                password: ['minLength[6]', 'empty'],
+                email: 'empty',
+                gender: 'empty',
+                terms: 'checked'
+            }
+        });
 }
 
-$(document).ready(function(){
-$('#toggle').click(function(){
-  $('.ui.sidebar').sidebar('toggle');
-});
+$(document).ready(function() {
+    $('#toggle').click(function() {
+        $('.ui.sidebar').sidebar('toggle');
+    });
 })
 
 
-$(document).ready(function(){
+$(document).ready(function() {
     $('.done').modal('show');
 })
 
-$(document).ready(function(){
+$(document).ready(function() {
     $('.sign').modal('show');
 })
 
 function done() {
-  return true;
+    return true;
 }
